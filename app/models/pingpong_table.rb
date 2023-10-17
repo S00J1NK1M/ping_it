b176781daf87e8a2267065495aeb9ab2d1d062af
eric sunza <e.sunza@web.de>
2023-10-17 11:13:57 +0200

    change model and create geocoder

--- app/models/pingpong_table.rb
+++ app/models/pingpong_table.rb
@@ -1,4 +1,6 @@
 class PingpongTable < ApplicationRecord
-    has_many :matches 
-    has_many :reservation
+  has_many :matches 
+  has_many :reservation
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
