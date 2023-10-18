c73fa4526b701ac6922f6b2bd0e57552e97a9c5e
Matteo Florio <matteoflorio16@gmail.com>
2023-10-18 12:39:49 +0200

    nickname parameter added to devise

--- app/controllers/pingpong_tables_controller.rb
+++ app/controllers/pingpong_tables_controller.rb
@@ -1,4 +1,20 @@
 class PingpongTablesController < ApplicationController
   def index
+    @pingpong_tables = PingpongTable.all
+
+    @markers = @pingpong_tables.geocoded.map do |pingpong_table|
+      {
+        latitude: pingpong_table.latitude,
+        longitude: pingpong_table.longitude
+      }
+    end
   end
+
+  def show
+  end
+
+  def create
+  end
+
+
 end
