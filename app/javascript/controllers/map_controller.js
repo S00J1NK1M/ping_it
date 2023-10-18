338c98c02d7ab76fd77f46a2936eacf625204369
Matteo Florio <matteoflorio16@gmail.com>
2023-10-18 12:53:39 +0200

    map added in stimulus controller

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,6 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
+import mapboxgl from 'mapbox-gl'
+
 
 // Connects to data-controller="map"
 export default class extends Controller {
-  connect() {console.log("Hello Pingersss!")  }
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
+
+  connect() {
+    mapboxgl.accessToken = this.apiKeyValue
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10"
+    })
+  }
 }
