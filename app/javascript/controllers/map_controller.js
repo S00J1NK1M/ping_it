65354d8c43a6c0c50116ba94e6dddaec71b6610a
Matteo Florio <matteoflorio16@gmail.com>
2023-10-18 15:16:30 +0200

    markers fit bounds added to map stimulus controller

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -10,11 +10,26 @@ export default class extends Controller {
   }
 
   connect() {
+    console.log(this.markersValue)
+
+
     mapboxgl.accessToken = this.apiKeyValue
+
 
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
+
+    this.markersValue.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.longitude, marker.latitude ])
+        .addTo(this.map)
+    })
+
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.longitude, marker.latitude ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+
   }
 }
