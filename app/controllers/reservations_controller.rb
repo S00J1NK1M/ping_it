f663f119c42db1ddda544349de99d446e6b7eddc
eric sunza <e.sunza@web.de>
2023-10-24 11:49:44 +0200

    added qr hidden in show as well as play method

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,53 +1,54 @@
 class ReservationsController < ApplicationController
-    def index
-        @reservations = Reservation.all
+  def index
+    @reservations = Reservation.all
         # A user can reserve one table only - how can we limit that?
-      end
+  end
     
-      def new
-        @reservation = Reservation.new
+  def new
+    @reservation = Reservation.new
       end
-    
-      def create
-        @pingpong_table = PingpongTable.find(params[:pingpong_table_id])
-        @pingpong_table.update(availability: false)
-        @reservation = Reservation.new
 
-       
-    
-        @reservation.pingpong_table = @pingpong_table
-    
+  def create
+    @pingpong_table = PingpongTable.find(params[:pingpong_table_id])
+    @pingpong_table.update(availability: false)
+    @reservation = Reservation.new
+
+    @reservation.pingpong_table = @pingpong_table
         # Find the current user
-        @reservation.user = current_user
-    
-        # Save
-        @reservation.save
-    
-        # Go to reservations#show
-        redirect_to pingpong_table_reservation_path(@pingpong_table, @reservation)
-      end
-    
-      def update
-      end
+    @reservation.user = current_user
 
-      def show
-        @pingpong_table = Reservation.find(params[:id]).pingpong_table
+    # Save
+    @reservation.save
+    
+    # Go to reservations#show
+    redirect_to pingpong_table_reservation_path(@pingpong_table, @reservation)
+    end
+    
+    def update
+    end
+
+    def show
+      @pingpong_table = Reservation.find(params[:id]).pingpong_table
+
+      
+    url = "#{request.base_url}#{play_pingpong_table_path(params[:pingpong_table_id], res:params[:id])}"
+    
         
-        @qr = RQRCode::QRCode.new(taken_pingpong_table_path(params[:pingpong_table_id], res:params[:id]))
+    @qr = RQRCode::QRCode.new(url)
     
-        @svg = @qr.as_svg(
-         color: :black,
-         shape_rendering: "crispEdges",
-         module_size: 11,
-         standalone: true,
-         use_path: true
-       )
+    @svg = @qr.as_svg(
+        color: :black,
+        shape_rendering: "crispEdges",
+        module_size: 11,
+        standalone: true,
+        use_path: true
+        )
 
-      end
-    
-      private
-    
-      def reservation_params
-        params.require(:reservation).permit(:user_id, :pingpong_table_id)
-      end
+    end
+
+    private
+
+    def reservation_params
+    params.require(:reservation).permit(:user_id, :pingpong_table_id)
+    end
 end
