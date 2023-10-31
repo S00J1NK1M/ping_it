ef197fecdc8294a14ef1881bb2e3eb3dd1e10172
Soojin Kim <contact.soojin.kim@gmail.com>
2023-10-31 10:25:32 +0100

    added seed for users and matches

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,65 @@ require "open-uri"
 
 PingpongTable.destroy_all
 
+user1 = User.new({
+  email: "soojin@kim.com",
+  password: "111111",
+  password_confirmation: "111111",
+  reset_password_token: nil,
+  reset_password_sent_at: nil,
+  remember_created_at: nil,
+  nickname: "PingPongQueen",
+  address: "kimstr. 1",
+  score: nil
+})
+
+user1.save!
+
+user2 = User.new({
+  email: "matteo@florio.com",
+  password: "111111",
+  password_confirmation: "111111",
+  reset_password_token: nil,
+  reset_password_sent_at: nil,
+  remember_created_at: nil,
+  nickname: "PingPongKing",
+  address: "floriostr. 1",
+  score: nil
+})
+
+user2.save!
+
+user3 = User.new({
+  email: "anna@boiko.com",
+  password: "111111",
+  password_confirmation: "111111",
+  reset_password_token: nil,
+  reset_password_sent_at: nil,
+  remember_created_at: nil,
+  nickname: "AnnaBanana",
+  address: "annastr. 1",
+  score: nil
+})
+
+user3.save!
+
+user4 = User.new({
+  email: "erica@sunza.com",
+  password: "111111",
+  password_confirmation: "111111",
+  reset_password_token: nil,
+  reset_password_sent_at: nil,
+  remember_created_at: nil,
+  nickname: "EricInYourFace",
+  address: "ericstr. 1",
+  score: nil
+})
+
+user4.save!
+
+
+p "Created users"
+
 # urls = ["https://images.unsplash.com/photo-1519871195344-9d1a2379cea0?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHBpbmclMjBwb25nJTIwdGFibGV8ZW58MHx8MHx8fDA%3D", "https://images.unsplash.com/photo-1615177981645-51107b202210?auto=format&fit=crop&q=80&w=2827&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"]
 
 # file = URI.open()
@@ -83,3 +142,90 @@ ping_pong_table_five.photo.attach(io: file, filename: "ping3.jpg", content_type:
 ping_pong_table_five.save!
 
 p "Created #{PingpongTable.count} PingpongTables"
+
+match1_time = DateTime.now + (rand * 21)
+match2_time = DateTime.now + (rand * 21)
+match3_time = DateTime.now + (rand * 21)
+match4_time = DateTime.now + (rand * 21)
+match5_time = DateTime.now + (rand * 21)
+match6_time = DateTime.now + (rand * 21)
+
+match1 = Match.new({
+  name: "Fun Match in Kreuzberg",
+  match_time: match1_time,
+  location: "Prinzenstraße 13, 10969 Berlin",
+  equipment: true,
+  organiser_id: User.all.sample.id,
+  buddy_id: nil,
+  winner_id: nil,
+  match_status: nil
+})
+
+match1.save!
+
+match2 = Match.new({
+  name: "TikiTaka match",
+  match_time: match2_time,
+  location: "Lindenstraße 74, 10969 Berlin",
+  equipment: true,
+  organiser_id: User.all.sample.id,
+  buddy_id: nil,
+  winner_id: nil,
+  match_status: nil
+})
+
+match2.save!
+
+match3 = Match.new({
+  name: "Let's burn calories",
+  match_time: match3_time,
+  location: "Friedrichstraße 224, 10969 Berlin",
+  equipment: true,
+  organiser_id: User.all.sample.id,
+  buddy_id: nil,
+  winner_id: nil,
+  match_status: nil
+})
+
+match3.save!
+
+match4 = Match.new({
+  name: "Feierabend match",
+  match_time: match4_time,
+  location: "Ritterstraße 53, 10969 Berlin",
+  equipment: true,
+  organiser_id: User.all.sample.id,
+  buddy_id: nil,
+  winner_id: nil,
+  match_status: nil
+})
+
+match4.save!
+
+match5 = Match.new({
+  name: "Stress-free playtime",
+  match_time: match5_time,
+  location: "Hallesches Ufer 70, 10963 Berlin",
+  equipment: true,
+  organiser_id: User.all.sample.id,
+  buddy_id: nil,
+  winner_id: nil,
+  match_status: nil
+})
+
+match5.save!
+
+match6 = Match.new({
+  name: "Ultimate Berliner game",
+  match_time: match6_time,
+  location: "Möckernstrasse 120, 10963 Berlin",
+  equipment: true,
+  organiser_id: User.all.sample.id,
+  buddy_id: nil,
+  winner_id: nil,
+  match_status: nil
+})
+
+match6.save!
+
+p "Created matches"
