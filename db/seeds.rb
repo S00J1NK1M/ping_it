6657dc99dfe15ed919ffb9c2ae3aeba697ef9ca2
Soojin Kim <contact.soojin.kim@gmail.com>
2023-11-01 12:02:53 +0100

    added new image in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -168,7 +168,7 @@ ping_pong_table_7 = PingpongTable.new({
   availability: true
 })
 
-file = URI.open("https://unsplash.com/ko/%EC%82%AC%EC%A7%84/%EB%9D%BC%EC%BC%93%EC%9D%84-%EB%93%A0-%EB%91%90-%EB%AA%85%EC%9D%98-%ED%99%9C%EB%8F%99%EC%A0%81%EC%9D%B8-%EB%85%B8%EC%9D%B8%EC%9D%B4-%ED%99%80%EC%9D%98-%ED%81%B0-%ED%85%8C%EC%9D%B4%EB%B8%94-%EC%98%86%EC%97%90%EC%84%9C-%ED%83%81%EA%B5%AC%EB%A5%BC-%EC%B9%98%EA%B3%A0-%EC%9E%88%EC%8A%B5%EB%8B%88%EB%8B%A4-3D18bJTKLzg")
+file = URI.open("https://plus.unsplash.com/premium_photo-1664298217898-c62231a2e47b?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
 ping_pong_table_7.photo.attach(io: file, filename: "ping7.jpg", content_type: "image/jpg")
 ping_pong_table_7.save!
 
